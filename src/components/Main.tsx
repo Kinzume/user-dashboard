3ca311d0249552c9eb1592f9a384a6afdec2b635
Kinzume <81969626+Kinzume@users.noreply.github.com>
2023-01-19 22:21:59 +0000

    feat: add section2

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,14 @@
 import { styled } from "@mui/material";
-import Section1 from "./Section1";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
+import Section1 from "./Section1";
+import Section2 from "./Section2/Section2";
 export default function Main() {
   return (
     <Box component="main">
       <Container>
         <Section1 />
+        <Section2 />
       </Container>
     </Box>
   );
