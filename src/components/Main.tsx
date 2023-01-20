eccb3cef783ea0667dde5d85f627f8aad43920a0
Kinzume <81969626+Kinzume@users.noreply.github.com>
2023-01-20 11:22:20 +0000

    feat(Main): add grid

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,14 +1,29 @@
 import { styled } from "@mui/material";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
 import Section1 from "./Section1";
 import Section2 from "./Section2/Section2";
 export default function Main() {
   return (
     <Box component="main">
       <Container>
-        <Section1 />
-        <Section2 />
+        <Grid container>
+          <Grid
+            item
+            xs={12}
+            md={8}
+          >
+            <Section1 />
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            md={4}
+          >
+            <Section2 />
+          </Grid>
+        </Grid>
       </Container>
     </Box>
   );
