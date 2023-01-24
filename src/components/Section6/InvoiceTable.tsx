c744ccd0ccac7feff815d41a4aeade977653b8e1
Kinzume <81969626+Kinzume@users.noreply.github.com>
2023-01-24 11:54:50 +0000

    fix(section6): update table and add call to action

---
+++ src/components/Section6/InvoiceTable.tsx
@@ -0,0 +1,64 @@
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import MoreOptions from "./MoreOptions";
+import Status from "./Status";
+
+type data = { ID: string; Category: string; Price: string; Status: string };
+const data = [
+  { ID: "INV-4868", Category: "Windows", Price: "$120.45", Status: "Paid" },
+  { ID: "INV-4869", Category: "Mac", Price: "$78.30", Status: "In Progress" },
+  {
+    ID: "INV-4870",
+    Category: "Android",
+    Price: "$56.78",
+    Status: "Out Of Date",
+  },
+  { ID: "INV-4871", Category: "IOS", Price: "$128.50", Status: "In Progress" },
+  { ID: "INV-4872", Category: "Apple", Price: "$36.78", Status: "Paid" },
+];
+
+export default function InvoiceTable() {
+  return (
+    <TableContainer
+      component={Paper}
+      sx={{ boxShadow: "none" }}
+    >
+      <Table aria-label="Item Data">
+        <TableHead>
+          <TableRow>
+            <TableCell>Invoice ID</TableCell>
+            <TableCell>Category</TableCell>
+            <TableCell>Price</TableCell>
+            <TableCell>Status</TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data?.map((invoice) => (
+            <TableRow key={invoice?.ID}>
+              <TableCell
+                component="th"
+                scope="row"
+              >
+                {invoice?.ID}
+              </TableCell>
+              <TableCell>{invoice?.Category}</TableCell>
+              <TableCell>{invoice?.Price}</TableCell>
+              <TableCell>
+                <Status status={invoice?.Status} />
+              </TableCell>
+              <TableCell>
+                <MoreOptions />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- src/components/Section6/Section6.tsx
+++ src/components/Section6/Section6.tsx
@@ -1,103 +1,28 @@
-import * as React from "react";
-import { useState } from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
+import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-import IconButton from "@mui/material/IconButton";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import MenuList from "@mui/material/MenuList";
-import ListItemText from "@mui/material/ListItemText";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import DownloadIcon from "@mui/icons-material/Download";
-import PrintIcon from "@mui/icons-material/Print";
-import ShareIcon from "@mui/icons-material/Share";
-import DeleteIcon from "@mui/icons-material/Delete";
+import Typography from "@mui/material/Typography";
+import InvoiceTable from "./InvoiceTable";
+import Button from "@mui/material/Button";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 export default function Section6() {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
   return (
-    <TableContainer component={Paper}>
-      <Table aria-label="Item Data">
-        <TableHead>
-          <TableRow>
-            <TableCell>Invoice ID</TableCell>
-            <TableCell>Category</TableCell>
-            <TableCell>Price</TableCell>
-            <TableCell>Status</TableCell>
-            <TableCell></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <TableRow>
-            <TableCell
-              component="th"
-              scope="row"
-            >
-              INV-4868
-            </TableCell>
-            <TableCell>Windows</TableCell>
-            <TableCell>$78.30</TableCell>
-            <TableCell>Paid</TableCell>
-            <TableCell>
-              <IconButton
-                aria-controls={open ? "menu" : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? "true" : undefined}
-                onClick={handleClick}
-                aria-label="More Options"
-              >
-                <MoreVertIcon />
-              </IconButton>
-              <Menu
-                id="menu"
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                MenuListProps={{
-                  "aria-labelledby": "basic-button",
-                }}
-              >
-                <MenuItem onClick={handleClose}>
-                  <ListItemIcon>
-                    <DownloadIcon fontSize="small" />
-                  </ListItemIcon>
-                  Download
-                </MenuItem>
-                <MenuItem onClick={handleClose}>
-                  <ListItemIcon>
-                    <PrintIcon fontSize="small" />
-                  </ListItemIcon>
-                  Print
-                </MenuItem>
-                <MenuItem onClick={handleClose}>
-                  <ListItemIcon>
-                    <ShareIcon fontSize="small" />
-                  </ListItemIcon>
-                  Share
-                </MenuItem>
-                <MenuItem onClick={handleClose}>
-                  <ListItemIcon>
-                    <DeleteIcon fontSize="small" />
-                  </ListItemIcon>
-                  Delete
-                </MenuItem>
-              </Menu>
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Paper
+      elevation={3}
+      sx={{ borderRadius: 4, display: "flex", flexDirection: "column" }}
+    >
+      <Typography sx={{ fontWeight: 500, typography: "h6", p: 2 }}>
+        New Invoice
+      </Typography>
+      <InvoiceTable />
+      <Box sx={{ p: 1, marginLeft: "auto" }}>
+        <Button
+          variant="text"
+          endIcon={<ArrowForwardIosIcon />}
+          sx={{ width: "fit-content", p: 1, borderRadius: 2 }}
+        >
+          View All
+        </Button>
+      </Box>
+    </Paper>
   );
 }
