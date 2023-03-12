8adff608de2f89ff59cd984841d582066d68f9a0
Kinzume <81969626+Kinzume@users.noreply.github.com>
2023-03-12 12:44:50 +0000

    refactor sx props

--- src/components/CallToAction/CallToAction.tsx
+++ src/components/CallToAction/CallToAction.tsx
@@ -1,35 +1,36 @@
 import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { ReactComponent as Logo } from "../../assets/undraw_dashboard_re_3b76.svg";
 
-const boxSx = {
+const boxWrapperSx = {
   height: "100%",
   display: "flex",
   flexDirection: { xs: "column", md: "row" },
   borderRadius: 4,
   background: "linear-gradient(135deg, #4d3fb54d, #4d3fb587)",
   overflow: "hidden",
+  color: "primary.dark",
 };
+const stackWrapperSx = { flexGrow: 1, p: 4 };
+const buttonSx = { textTransform: "none", borderRadius: 2, fontWeight: 700 };
+const boxLogoSx = { marginInline: "auto", p: 6, width: 360 };
 
 export default function CallToAction() {
   return (
-    <Box sx={boxSx}>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: { xs: "center", md: "start" },
-          flexGrow: 1,
-          justifyContent: "center",
-          p: 4,
-        }}
+    <Box sx={boxWrapperSx}>
+      <Stack
+        direction="column"
+        alignItems={{ xs: "center", md: "start" }}
+        justifyContent="center"
+        sx={stackWrapperSx}
       >
         <Typography
-          component={"h2"}
+          component="h2"
           variant="h5"
           paragraph
-          sx={{ color: "primary.dark", fontWeight: 500, mb: 0 }}
+          sx={{ mb: 0 }}
           gutterBottom
         >
           Welcome Back!
@@ -37,18 +38,14 @@ export default function CallToAction() {
         <Typography
           variant="h5"
           paragraph
-          sx={{ color: "primary.dark", fontWeight: 500 }}
+          sx={{ fontWeight: 500 }}
           gutterBottom
         >
           Minimal UI Clone
         </Typography>
         <Typography
           variant="body2"
-          sx={{
-            color: "primary.dark",
-            textAlign: { xs: "center", md: "start" },
-            mb: 4,
-          }}
+          sx={{ textAlign: { xs: "center", md: "start" }, mb: 4 }}
         >
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident,
           exercitationem.
@@ -57,16 +54,12 @@ export default function CallToAction() {
           variant="contained"
           disableElevation
           size="medium"
-          sx={{
-            textTransform: "none",
-            borderRadius: 2,
-            fontWeight: 700,
-          }}
+          sx={buttonSx}
         >
           Go Now
         </Button>
-      </Box>
-      <Box sx={{ marginInline: "auto", p: 6, width: 360 }}>
+      </Stack>
+      <Box sx={boxLogoSx}>
         <Logo />
       </Box>
     </Box>
